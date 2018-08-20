2f974c8501153e7acd50b21f3cef9e9df8790d7d
Hanna Kandratsyeva <hanna.kandratsyeva@itechart-group.com>
2018-08-20 16:43:07 +0300

    release 1

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -2,6 +2,7 @@ import * as axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'http://10.0.2.2:3001/',
+    //baseURL: 'https://react-films2.herokuapp.com/',
     timeout: 20000,
     headers: {
         'Content-Type': 'application/json'
