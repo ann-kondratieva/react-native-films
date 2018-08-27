cdf093e55d8db25b45d1592169f433da7dcad977
Hanna Kandratsyeva <hanna.kandratsyeva@itechart-group.com>
2018-08-27 10:53:05 +0300

    fix firebase

--- client/src/services/firebase.js
+++ client/src/services/firebase.js
@@ -2,9 +2,7 @@ import firebase from 'firebase';
 import ReduxSagaFirebase from 'redux-saga-firebase';
 
 firebase.initializeApp({
-    apiKey: 'AIzaSyAkkeQYYMX8rr_FWSHig9jGQJnVJ81jg2U',
-    projectId: 'reactfilms-853e9',
-    storageBucket: 'gs://reactfilms-853e9.appspot.com'
+
 });
 
 export const reduxSagaFirebase = new ReduxSagaFirebase(firebase);
